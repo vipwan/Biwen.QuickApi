e01936d500676223d66103b1d26973fe5e30c5f2
万雅虎 <vipwan@outlook.com>
2023-09-21 03:04:52 +0800

    Demo

--- Biwen.QuickApi.DemoWeb/Program.cs
+++ Biwen.QuickApi.DemoWeb/Program.cs
@@ -1,5 +1,6 @@
 using Biwen.QuickApi;
 using Biwen.QuickApi.DemoWeb;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,10 +28,22 @@ app.MapBiwenQuickApis();
 //测试其他地方调用QuickApi
 app.MapGet("/fromapi", (Biwen.QuickApi.DemoWeb.Apis.Hello4Api api) =>
 {
+    //通过你的方式获取请求对象
+    var req = new EmptyRequest();
+    //验证请求对象
+    var validator = req.RealValidator as IValidator<EmptyRequest>;
+    if (validator != null)
+    {
+        var result = validator.Validate(req);
+        if (!result.IsValid)
+        {
+            return Results.BadRequest(result.ToDictionary());
+        }
+    }
+    //执行请求
     var x = api.Execute(new EmptyRequest());
     return Results.Ok(x);
 });
-
 
 
 app.Run();
