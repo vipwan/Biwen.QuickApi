d05bb959aa458d034d338402575b7f4b7de859c2
万雅虎 <vipwan@outlook.com>
2023-10-11 15:20:14 +0800

    doc

--- Biwen.QuickApi/Attributes/AliasAsAttribute.cs
+++ Biwen.QuickApi/Attributes/AliasAsAttribute.cs
@@ -5,7 +5,7 @@ namespace Biwen.QuickApi.Attributes
 
     /// <summary>
     /// 针对属性的别名,
-    /// 如果是自定义ReqBinder,需要自行处理
+    /// 一般用于Rsp,如果需要用于Req,请自定义Binder或者修改传输对象属性为Alias
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
     public class AliasAsAttribute : Attribute
