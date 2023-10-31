f1d310177faf5c1a9656a4c863ecf9f4348647e2
万雅虎 <vipwan@outlook.com>
2023-11-01 02:39:35 +0800

    code cleanup

--- Biwen.QuickApi/Attributes/AutoAnnotationAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Biwen.QuickApi.Attributes
-{
-    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = true)]
-    public sealed class AutoAnnotationAttribute : Attribute
-    {
-    }
-}
