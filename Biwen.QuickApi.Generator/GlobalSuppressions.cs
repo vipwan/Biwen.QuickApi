2790d41fdf7edba0fba0b32ba0564ee34678ddc4
万雅虎 <vipwan@outlook.com>
2024-05-27 15:37:36 +0800

    Delete Biwen.QuickApi.Generator directory

--- Biwen.QuickApi.Generator/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Globalization", "CA1304:指定 CultureInfo", Justification = "<挂起>", Scope = "member", Target = "~M:Biwen.QuickApi.SourceGenerator.StringExtensions.ToCamelCase(System.String)~System.String")]
-[assembly: SuppressMessage("Globalization", "CA1304:指定 CultureInfo", Justification = "<挂起>", Scope = "member", Target = "~M:Biwen.QuickApi.SourceGenerator.StringExtensions.ToPascalCase(System.String)~System.String")]
