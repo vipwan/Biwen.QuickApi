9bd65a82df6d7c18e17cd5a44ae83fbdf6044f73
万雅虎 <vipwan@outlook.com>
2024-05-27 15:37:49 +0800

    Delete Biwen.QuickApi.SourceGenerator.TestConsole directory

--- Biwen.QuickApi.SourceGenerator.TestConsole/_GloableUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using Biwen.QuickApi;
-global using Biwen.QuickApi.Attributes;
-global using Biwen.QuickApi.Metadata;
-global using Biwen.QuickApi.Http;
-global using Microsoft.AspNetCore.Antiforgery;
