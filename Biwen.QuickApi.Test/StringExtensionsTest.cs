f43eeaf8395d1538ff3b6b271ada667620eaa9af
万雅虎 <vipwan@outlook.com>
2024-06-09 15:50:08 +0800

    新增字符串扩展方法和对应的单元测试

---
+++ Biwen.QuickApi.Test/StringExtensionsTest.cs
@@ -0,0 +1,83 @@
+﻿using Xunit.Abstractions;
+
+namespace Biwen.QuickApi.Test
+{
+    public class StringExtensionsTest(ITestOutputHelper testOutput)
+    {
+        [Theory]
+        [InlineData('a', true)]
+        [InlineData('A', true)]
+        [InlineData('0', false)]
+        [InlineData('我', false)]
+        public void IsLetterTest(char letter, bool flag)
+        {
+            testOutput.WriteLine($"IsLetterTest: {letter} => {flag}");
+            Assert.Equal(flag, letter.IsLetter());
+        }
+
+        //测试是否空格
+        [Theory]
+        [InlineData(' ', true)]
+        [InlineData('\r', true)]
+        [InlineData('\n', true)]
+        [InlineData('\t', true)]
+        [InlineData('\f', true)]
+        [InlineData('a', false)]
+        public void IsSpaceTest(char letter, bool flag)
+        {
+            testOutput.WriteLine($"IsSpaceTest: {letter} => {flag}");
+            Assert.Equal(flag, letter.IsSpace());
+        }
+
+        //RemoveTags test
+        [Theory]
+        [InlineData("<p>hello</p>", "hello")]
+        [InlineData("<p>hello</p><p>world</p>", "helloworld")]
+        [InlineData("<p>hello</p><p>world</p><p>!</p>", "helloworld!")]
+        [InlineData("<p>hello</p><p>world</p><p>!</p><p> </p>", "helloworld! ")]
+        public void RemoveTagsTest(string html, string result)
+        {
+            testOutput.WriteLine($"RemoveTagsTest: {html} => {result}");
+            Assert.Equal(result, html.RemoveTags());
+        }
+
+        //ToPascalCase test
+        [Theory]
+        [InlineData("hello", "Hello", ' ')]
+        [InlineData("hello world", "HelloWorld", ' ')]
+        [InlineData("hello-world", "HelloWorld", '-')]
+        [InlineData("hello_world", "HelloWorld", '_')]
+        [InlineData("hello_world_", "HelloWorld", '_')]
+        public void ToPascalCaseTest(string text, string result, char upperAfterDelimiter)
+        {
+            testOutput.WriteLine($"ToPascalCaseTest: {text} => {result}");
+            Assert.Equal(result, text.ToPascalCase(upperAfterDelimiter));
+        }
+
+
+        //ToCamalCase test
+        [Theory]
+        [InlineData(" hello ", "hello")]
+        [InlineData("hello             world", "helloWorld")]
+        [InlineData("hello world      two", "helloWorldTwo")]
+        public void ToCamalCaseTest(string text, string result)
+        {
+            testOutput.WriteLine($"ToCamalCaseTest: {text} => {result}");
+            Assert.Equal(result, text.ToCamalCase());
+        }
+
+        //IsJson test
+        [Theory]
+        [InlineData("{\"name\":\"biwen\"}", true)]
+        [InlineData("hello world", false)]
+        [InlineData("[]", true)]
+        [InlineData("", false)]
+        public void IsJsonTest(string text, bool flag)
+        {
+            testOutput.WriteLine($"IsJsonTest: {text} => {flag}");
+            Assert.Equal(flag, text.IsJson());
+        }
+
+    }
+
+}
---
+++ Biwen.QuickApi/Extensions/StringExtensions.cs
@@ -0,0 +1,198 @@
+﻿using System.Text.Json.Nodes;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Net;
+namespace System;
+public static class StringExtensions
+{
+
+    /// <summary>
+    /// 判定是否为字母a-z,A-Z
+    /// </summary>
+    public static bool IsLetter(this char c)
+    {
+        return c is >= 'A' and <= 'Z' or >= 'a' and <= 'z';
+    }
+
+    /// <summary>
+    /// 判定是否为空格
+    /// </summary>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    public static bool IsSpace(this char c)
+    {
+        return c is '\r' or '\n' or '\t' or '\f' or ' ';
+    }
+
+    /// <summary>
+    /// 移除字符串中的html标签
+    /// </summary>
+    /// <param name="html"></param>
+    /// <param name="htmlDecode"></param>
+    /// <returns></returns>
+    public static string RemoveTags(this string html, bool htmlDecode = false)
+    {
+        if (string.IsNullOrEmpty(html))
+        {
+            return string.Empty;
+        }
+
+        var result = new char[html.Length];
+
+        var cursor = 0;
+        var inside = false;
+        for (var i = 0; i < html.Length; i++)
+        {
+            var current = html[i];
+
+            switch (current)
+            {
+                case '<':
+                    inside = true;
+                    continue;
+                case '>':
+                    inside = false;
+                    continue;
+            }
+
+            if (!inside)
+            {
+                result[cursor++] = current;
+            }
+        }
+
+        var stringResult = new string(result, 0, cursor);
+
+        if (htmlDecode)
+        {
+            stringResult = WebUtility.HtmlDecode(stringResult);
+        }
+
+        return stringResult;
+    }
+
+    /// <summary>
+    /// 将字符串转换为帕斯卡命名法
+    /// </summary>
+    public static string ToPascalCase(this string attribute, char upperAfterDelimiter = ' ')
+    {
+        attribute = attribute.Trim();
+
+        var delimitersCount = 0;
+
+        for (var i = 0; i < attribute.Length; i++)
+        {
+            if (attribute[i] == upperAfterDelimiter)
+            {
+                delimitersCount++;
+            }
+        }
+
+        var result = string.Create(attribute.Length - delimitersCount, new { attribute, upperAfterDelimiter }, (buffer, state) =>
+        {
+            var nextIsUpper = true;
+            var k = 0;
+
+            for (var i = 0; i < state.attribute.Length; i++)
+            {
+                var c = state.attribute[i];
+
+                if (c == state.upperAfterDelimiter)
+                {
+                    nextIsUpper = true;
+                    continue;
+                }
+
+                if (nextIsUpper)
+                {
+                    buffer[k] = char.ToUpperInvariant(c);
+                }
+                else
+                {
+                    buffer[k] = c;
+                }
+
+                nextIsUpper = false;
+
+                k++;
+            }
+        });
+
+        return result;
+    }
+
+    /// <summary>
+    /// 将字符串中的空格转换为驼峰
+    /// </summary>
+    /// <param name="attribute"></param>
+    /// <returns></returns>
+    public static string ToCamalCase(this string attribute)
+    {
+        if (string.IsNullOrEmpty(attribute))
+        {
+            return string.Empty;
+        }
+
+        //去除空格
+        attribute = attribute.Trim();
+
+        var result = new char[attribute.Length];
+
+        var cursor = 0;
+        var nextIsUpper = false;
+        for (var i = 0; i < attribute.Length; i++)
+        {
+            var current = attribute[i];
+
+            if (current == ' ')
+            {
+                nextIsUpper = true;
+                continue;
+            }
+
+            if (nextIsUpper)
+            {
+                result[cursor++] = char.ToUpperInvariant(current);
+            }
+            else
+            {
+                result[cursor++] = current;
+            }
+
+            nextIsUpper = false;
+        }
+
+        return new string(result, 0, cursor);
+
+    }
+
+
+    /// <summary>
+    /// 字符串替换
+    /// </summary>
+    /// <param name="original"></param>
+    /// <param name="replacements"></param>
+    /// <returns></returns>
+    public static string ReplaceAll(this string original, IDictionary<string, string> replacements)
+    {
+        var pattern = $"{string.Join("|", replacements.Keys)}";
+        return Regex.Replace(original, pattern, match => replacements[match.Value]);
+    }
+
+    /// <summary>
+    /// 判定是否为json字符串
+    /// </summary>
+    public static bool IsJson(this string json, JsonDocumentOptions jsonDocumentOptions = default)
+    {
+        try
+        {
+            JsonNode.Parse(json, null, jsonDocumentOptions);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+}
