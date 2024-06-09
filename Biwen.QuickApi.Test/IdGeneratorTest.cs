7a370c4e907645e2362b52a31ec6015e7879668d
万雅虎 <vipwan@outlook.com>
2024-06-09 12:56:35 +0800

    test

--- Biwen.QuickApi.Test/IdGeneratorTest.cs
+++ Biwen.QuickApi.Test/IdGeneratorTest.cs
@@ -1,14 +1,19 @@
-﻿namespace Biwen.QuickApi.Test
+﻿using Xunit.Abstractions;
+
+namespace Biwen.QuickApi.Test
 {
-    public class IdGeneratorTest
+    public class IdGeneratorTest(ITestOutputHelper testOutputHelper)
     {
+
         [Fact]
         public async Task IdGenTest()
         {
             var str = IdGenerator.GenerateId();
+
+            testOutputHelper.WriteLine($"Id:{str}");
+
             Assert.NotNull(str);
             Assert.True((str?.Length ?? 0) > 0);
-
             await Task.CompletedTask;
         }
     }
