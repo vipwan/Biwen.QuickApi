7284ea9bac01245c47e9af46724b42f2c53f3b12
万雅虎 <vipwan@outlook.com>
2024-06-25 16:05:43 +0800

    code cleanup

--- Biwen.QuickApi/Scheduling/SampleNCrontabScheduler.cs
+++ Biwen.QuickApi/Scheduling/SampleNCrontabScheduler.cs
@@ -4,7 +4,7 @@ using System.Collections.Concurrent;
 namespace Biwen.QuickApi.Scheduling
 {
     /// <summary>
-    /// 使用NCrontab实现的调度器,不支持秒级,这是默认调度器
+    /// 使用NCrontab实现的调度器,支持秒级(10s+),这是默认调度器
     /// </summary>
     public sealed class SampleNCrontabScheduler : IScheduler
     {
@@ -15,65 +15,60 @@ namespace Biwen.QuickApi.Scheduling
 
         public bool CanRun(ScheduleTaskAttribute scheduleMetadata, DateTime referenceTime)
         {
-            var (Flag, Seconds) = IsSecondLevel(scheduleMetadata.Cron);
-            //支持秒级别:
-            if (Flag)
-            {
-                var now = DateTime.Now;
-                var haveExcuteTime = LastRunTimes.TryGetValue(scheduleMetadata, out var time);
-                if (!haveExcuteTime)
-                {
-                    var nextStartTime = referenceTime.AddSeconds(Seconds);
-                    LastRunTimes.TryAdd(scheduleMetadata, nextStartTime);
+            var now = DateTime.Now;
+            var haveExcuteTime = LastRunTimes.TryGetValue(scheduleMetadata, out var time);
+            var nextStartTime = GetNextStartTime(scheduleMetadata, referenceTime);
 
-                    //如果不是初始化启动,则不执行
-                    if (!scheduleMetadata.IsStartOnInit)
-                        return false;
-                }
-                if (now >= time)
-                {
-                    var nextStartTime = referenceTime.AddSeconds(Seconds);
-                    //更新下次执行时间
-                    LastRunTimes.TryUpdate(scheduleMetadata, nextStartTime, time);
-                    return true;
-                }
-                return false;
+            if (!haveExcuteTime)
+            {
+                LastRunTimes.TryAdd(scheduleMetadata, nextStartTime);
+
+                //如果不是初始化启动,则不执行
+                if (!scheduleMetadata.IsStartOnInit)
+                    return false;
+            }
+
+            if (now >= time)
+            {
+                LastRunTimes.TryUpdate(scheduleMetadata, nextStartTime, time);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 获取下次执行时间
+        /// </summary>
+        /// <param name="scheduleMetadata"></param>
+        /// <param name="referenceTime"></param>
+        /// <returns></returns>
+        private DateTime GetNextStartTime(ScheduleTaskAttribute scheduleMetadata, DateTime referenceTime)
+        {
+            if (IsSecondLevel(scheduleMetadata.Cron, out var seconds))
+            {
+                return referenceTime.AddSeconds(seconds);
             }
             else
             {
-
-
-
-                var now = DateTime.Now;
-                var haveExcuteTime = LastRunTimes.TryGetValue(scheduleMetadata, out var time);
-                if (!haveExcuteTime)
-                {
-                    var nextStartTime = CrontabSchedule.Parse(scheduleMetadata.Cron).GetNextOccurrence(referenceTime);
-                    LastRunTimes.TryAdd(scheduleMetadata, nextStartTime);
-
-                    //如果不是初始化启动,则不执行
-                    if (!scheduleMetadata.IsStartOnInit)
-                        return false;
-                }
-                if (now >= time)
-                {
-                    var nextStartTime = CrontabSchedule.Parse(scheduleMetadata.Cron).GetNextOccurrence(referenceTime);
-                    //更新下次执行时间
-                    LastRunTimes.TryUpdate(scheduleMetadata, nextStartTime, time);
-                    return true;
-                }
-                return false;
+                return CrontabSchedule.Parse(scheduleMetadata.Cron).GetNextOccurrence(referenceTime);
             }
         }
-
-        private (bool Flag, int Seconds) IsSecondLevel(string cron)
+        /// <summary>
+        /// 判断是否是秒级
+        /// </summary>
+        /// <param name="cron"></param>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        private bool IsSecondLevel(string cron, out int seconds)
         {
             var cronArray = cron.Split(':');
-            if (cronArray.Length == 2 && cronArray[1] == "SECONDS")
+            if (cronArray.Length == 2 && cronArray[1] == "SECONDS" && int.TryParse(cronArray[0], out seconds))
             {
-                return (true, int.Parse(cronArray[0]));
+                return true;
             }
-            return (false, 0);
+            seconds = 0;
+            return false;
         }
     }
 }
