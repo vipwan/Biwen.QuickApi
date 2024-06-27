4241939ae2c5eb73757b2a754fe968a4df1ee564
万雅虎 <vipwan@outlook.com>
2024-06-28 03:02:01 +0800

    code cleanup

--- Biwen.QuickApi/UnitOfWork/AutoEventInterceptor.cs
+++ Biwen.QuickApi/UnitOfWork/AutoEventInterceptor.cs
@@ -15,21 +15,21 @@ internal class AutoEventInterceptor : ISaveChangesInterceptor
     public AutoEventInterceptor() { }
 
     #region SavedChanges
-    public InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    public InterceptionResult<int> SavingChanges(
+        DbContextEventData eventData, InterceptionResult<int> result)
     {
         var context = eventData.Context!;
-
-
         foreach (var entry in context.ChangeTracker.Entries())
         {
-            if (entry.Entity.GetType().GetCustomAttribute<AutoEventIgnoreAttribute>() is { })
+            var entityType = entry.Entity.GetType();
+            if (entityType.GetCustomAttribute<AutoEventIgnoreAttribute>() is { })
                 continue;
 
             _ = entry.State switch
             {
-                EntityState.Deleted => _publishDeletedAsync.MakeGenericMethod(entry.Entity.GetType()).Invoke(null, [entry.Entity]),
-                EntityState.Modified => _publishUpdatedAsync.MakeGenericMethod(entry.Entity.GetType()).Invoke(null, [entry.Entity]),
-                EntityState.Added => _publishAddedAsync.MakeGenericMethod(entry.Entity.GetType()).Invoke(null, [entry.Entity]),
+                EntityState.Deleted => _publishDeletedAsync.MakeGenericMethod(entityType).Invoke(null, [entry.Entity]),
+                EntityState.Modified => _publishUpdatedAsync.MakeGenericMethod(entityType).Invoke(null, [entry.Entity]),
+                EntityState.Added => _publishAddedAsync.MakeGenericMethod(entityType).Invoke(null, [entry.Entity]),
                 _ => null
             };
         }
@@ -37,20 +37,21 @@ internal class AutoEventInterceptor : ISaveChangesInterceptor
         return result;
     }
 
-    public async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    public async ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
     {
         var context = eventData.Context!;
-
         foreach (var entry in context.ChangeTracker.Entries())
         {
-            if (entry.Entity.GetType().GetCustomAttribute<AutoEventIgnoreAttribute>() is { })
+            var entityType = entry.Entity.GetType();
+            if (entityType.GetCustomAttribute<AutoEventIgnoreAttribute>() is { })
                 continue;
 
             _ = entry.State switch
             {
-                EntityState.Deleted => _publishDeletedAsync.MakeGenericMethod(entry.Entity.GetType()).Invoke(null, [entry.Entity]),
-                EntityState.Modified => _publishUpdatedAsync.MakeGenericMethod(entry.Entity.GetType()).Invoke(null, [entry.Entity]),
-                EntityState.Added => _publishAddedAsync.MakeGenericMethod(entry.Entity.GetType()).Invoke(null, [entry.Entity]),
+                EntityState.Deleted => _publishDeletedAsync.MakeGenericMethod(entityType).Invoke(null, [entry.Entity]),
+                EntityState.Modified => _publishUpdatedAsync.MakeGenericMethod(entityType).Invoke(null, [entry.Entity]),
+                EntityState.Added => _publishAddedAsync.MakeGenericMethod(entityType).Invoke(null, [entry.Entity]),
                 _ => null
             };
         }
@@ -59,9 +60,13 @@ internal class AutoEventInterceptor : ISaveChangesInterceptor
         return result;
     }
 
+    #region private
+
     private static readonly MethodInfo _publishAddedAsync = typeof(EntityExtensions).GetMethod(nameof(EntityExtensions.PublishAddedAsync))!;
     private static readonly MethodInfo _publishUpdatedAsync = typeof(EntityExtensions).GetMethod(nameof(EntityExtensions.PublishUpdatedAsync))!;
     private static readonly MethodInfo _publishDeletedAsync = typeof(EntityExtensions).GetMethod(nameof(EntityExtensions.PublishDeletedAsync))!;
+
+    #endregion
 
     #endregion
 }
