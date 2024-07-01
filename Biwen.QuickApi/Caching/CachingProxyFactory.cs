161efc77cfd7fa36d7080a8117e9d7d3baf7bd9a
万雅虎 <vipwan@outlook.com>
2024-07-01 13:07:40 +0800

    add CachingProxyFactory

--- Biwen.QuickApi/Caching/CachingProxyFactory.cs
+++ Biwen.QuickApi/Caching/CachingProxyFactory.cs
@@ -1,11 +1,14 @@
 ﻿namespace Biwen.QuickApi.Caching;
 
-public class CachingProxyFactory<T>(IServiceProvider serviceProvider)
-    where T : class
+/// <summary>
+/// 提供 <typeparamref name="T"/> 的缓存代理服务工厂,构造的代理会自动缓存方法的返回值
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <param name="serviceProvider"></param>
+public class CachingProxyFactory<T>(IServiceProvider serviceProvider) where T : class
 {
-
     /// <summary>
-    /// 创建代理实例
+    /// 根据实例创建代理
     /// </summary>
     /// <param name="impl"></param>
     /// <returns></returns>
@@ -16,7 +19,7 @@ public class CachingProxyFactory<T>(IServiceProvider serviceProvider)
     }
 
     /// <summary>
-    /// 创建容器中的服务缓存代理
+    /// 从DI容器中创建代理
     /// </summary>
     /// <returns></returns>
     public T Create()
