4e83f188eefb2b48f3b338b874ce192e2a92aa2f
万雅虎 <vipwan@outlook.com>
2024-07-16 15:07:48 +0800

    UUID7

--- Biwen.QuickApi.Test/IdGeneratorTest.cs
+++ Biwen.QuickApi.Test/IdGeneratorTest.cs
@@ -14,5 +14,21 @@
             Assert.True((str?.Length ?? 0) > 0);
             await Task.CompletedTask;
         }
+
+
+        [Fact]
+        public void Uuid7_Test()
+        {    
+            testOutputHelper.WriteLine(Uuid7.NewUuid4().ToString());
+
+            var id1 = Uuid7.NewUuid7();
+            var id2 = Uuid7.NewUuid7();
+
+            testOutputHelper.WriteLine($"{id1} {id2}");
+
+            Assert.True(id1 < id2);
+
+        }
+
     }
 }
